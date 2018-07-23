646f5f9580d3cae5333737290efc28d4f1bbb5c1
mmarszal <mmarszal@uascools.org>
2018-07-23 00:51:09 -0400

    display limit

--- app/src/main/java/com/example/kushtimusprime/cbusofficialtrialsix/HomeFragment.java
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialsix/HomeFragment.java
@@ -9,12 +9,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import org.w3c.dom.Document;
 
@@ -30,6 +34,8 @@ public class HomeFragment extends Fragment {
     private List<BlogPost> blogPosts=new ArrayList<BlogPost>();
     private FirebaseFirestore firebaseFirestore;
     private BlogRecyclerAdapter blogRecyclerAdapter;
+    private FirebaseAuth firebaseAuth;
+    private DocumentSnapshot lastVisible;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -41,24 +47,62 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home, container, false);
         blogPostView=view.findViewById(R.id.blogPostView);
+        firebaseAuth = FirebaseAuth.getInstance();
         blogRecyclerAdapter=new BlogRecyclerAdapter(blogPosts);
         blogPostView.setLayoutManager(new LinearLayoutManager(getActivity()));
         blogPostView.setAdapter(blogRecyclerAdapter);
+        if(firebaseAuth.getCurrentUser() != null){
         firebaseFirestore=FirebaseFirestore.getInstance();
-        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        blogPostView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Boolean reachedBottom = !recyclerView.canScrollVertically(1);
+                if(reachedBottom) {
+                    //theres supposed to be a toast here but it's causing trouble so i took it out
+                    loadMorePosts();
+                }
+            }
+        });
+
+        Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(5);
+        firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
-                for(DocumentChange doc:documentSnapshots.getDocumentChanges()) {
-                    if(doc.getType()==DocumentChange.Type.ADDED) {
-                        BlogPost blogPost=doc.getDocument().toObject(BlogPost.class);
+                lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size()-1);
+                for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                    if (doc.getType() == DocumentChange.Type.ADDED) {
+                        BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
                         blogPosts.add(blogPost);
                         blogRecyclerAdapter.notifyDataSetChanged();
                     }
                 }
             }
-        });
+            });
+        }
         // Inflate the layout for this fragment
         return view;
+    }
+    //the method below is from part 14 and is supposed to take you to the next page since a set number of posts
+    //which I set to 5 is displayed. we could either take this out or use it to rotate the pins if the user
+    //wants to see other ones?
+    public void loadMorePosts(){
+        Query nextQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).startAfter(lastVisible).limit(5);
+        nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (!documentSnapshots.isEmpty()) {
+                    lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blogPosts.add(blogPost);
+                            blogRecyclerAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        });
     }
 
 }

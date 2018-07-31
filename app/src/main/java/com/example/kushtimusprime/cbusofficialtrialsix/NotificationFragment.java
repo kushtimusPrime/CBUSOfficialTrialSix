44fbb8c49b3e19a7dd20e5bf15ba58aca961d58d
kushtimusPrime <kushhari007@gmail.com>
2018-07-30 22:58:54 -0400

    Map Fragment works

--- app/src/main/java/com/example/kushtimusprime/cbusofficialtrialsix/NotificationFragment.java
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialsix/NotificationFragment.java
@@ -2,17 +2,28 @@ package com.example.kushtimusprime.cbusofficialtrialsix;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NotificationFragment extends Fragment {
-
+public class NotificationFragment extends Fragment implements OnMapReadyCallback{
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -23,7 +34,26 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        mView=inflater.inflate(R.layout.fragment_notification, container, false);
+        return mView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView=(MapView)mView.findViewById(R.id.map);
+        if(mMapView!=null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        
+    }
 }
